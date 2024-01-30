2bf8015f4132cd178bcf090c2de5986af478241e
MasaKagami <97225192+MasaKagami@users.noreply.github.com>
2024-01-30 10:47:56 -0500

    Updates on masakagami.com

--- portfolio/src/components/NavBar/navbar.js
+++ portfolio/src/components/NavBar/navbar.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './navbar.css';
+import logo from '../../assets/logo.png'
+import { Link } from 'react-scroll';
 
 const Navbar = () => {
   return (
         <nav className="navbar">
-            img
+            <img src={logo} alt="<ogo" clasName='logo'/>
+            <div className="desktopMenu">
+              <Link className="desktopMenuListItem">Home</Link>
+              <Link className="desktopMenuListItem">About</Link>
+              <Link className="desktopMenuListItem">Skills</Link>
+              <Link className="desktopMenuListItem">Portfolio</Link>
+              <Link className="desktopMenuListItem">Contact</Link>
+
+
+            </div>
+            <button className="desktopMenuBtn">
+              <img src="" alt="" className="desktopMenuImg" />Contact Me</button>
         </nav>
     )
 }
