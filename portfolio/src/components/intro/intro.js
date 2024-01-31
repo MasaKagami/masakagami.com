08ef7751b5715a69d0daec83e7f562025d7e048b
MasaKagami <97225192+MasaKagami@users.noreply.github.com>
2024-01-30 21:01:13 -0500

    update

---
+++ portfolio/src/components/intro/intro.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './import.css';
+import bg from '../../assets/image.png';
+
+const Intro = () => {
+  return (
+    <section id="intro">
+        <div className="introContent">
+
+        </div>
+        <img src={bg} alt='Profile' className='bg'/>
+    </section>
+  )
+}
+
+export default Intro
