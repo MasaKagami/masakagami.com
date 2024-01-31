6fd88fa190ffcc480948290cb2e4d662b8c934a6
MasaKagami <97225192+MasaKagami@users.noreply.github.com>
2024-01-30 19:22:02 -0500

    updated

--- portfolio/src/components/NavBar/navbar.js
+++ portfolio/src/components/NavBar/navbar.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import './navbar.css';
-import logo from '../../assets/logo.png'
+import logo from '../../../src/assets/logo.png'
+import contactImg from '../../../src/assets/contact.png'
 import { Link } from 'react-scroll';
 
 const Navbar = () => {
   return (
         <nav className="navbar">
-            <img src={logo} alt="<ogo" clasName='logo'/>
+            <img src={logo} alt="Logo" className='logo'/>
             <div className="desktopMenu">
               <Link className="desktopMenuListItem">Home</Link>
               <Link className="desktopMenuListItem">About</Link>
               <Link className="desktopMenuListItem">Skills</Link>
               <Link className="desktopMenuListItem">Portfolio</Link>
-              <Link className="desktopMenuListItem">Contact</Link>
-
-
             </div>
             <button className="desktopMenuBtn">
-              <img src="" alt="" className="desktopMenuImg" />Contact Me</button>
+              <img src={contactImg} alt="contactImage" className="desktopMenuImg" />Contact Me</button>
         </nav>
     )
 }
