c86b59d790105a1cfab7bf020173567815a3f35c
MasaKagami <97225192+MasaKagami@users.noreply.github.com>
2024-06-03 19:05:53 -0400

    yur

--- portfolio/src/components/Skills/skills.js
+++ portfolio/src/components/Skills/skills.js
@@ -43,7 +43,7 @@ const Skills = () => {
             </div>
           </div>
         </div> */}
-
+      
         
     </section>
   )
---
+++ portfolio/src/components/outro/outro.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './outro.css';
+
+const Outro = () => {
+  return (
+    <div>Outro</div>
+  )
+}
+
+export default Outro
