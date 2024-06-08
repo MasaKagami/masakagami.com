19853411acff7cde23e52ea7b839fcd4615d554f
MasaKagami <97225192+MasaKagami@users.noreply.github.com>
2024-06-08 08:47:28 -0400

    Update App.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,7 +2,7 @@ import Navbar from "./components/NavBar/navbar";
 import Intro from "./components/intro/intro";
 import Skills from "./components/Skills/skills";
 import Works from "./components/Works/works";
-import Outro from "./components/Outro/outro"
+import Outro from "./components/outro/outro"
 import Footer from "./components/Footer/footer";
 
 function App() {
