585ffc0b80a6fce0556b6c623a218ed8be44770f
MasaKagami <97225192+MasaKagami@users.noreply.github.com>
2024-06-08 10:42:48 -0400

    Update MusicRedirect.js

--- portfolio/src/MusicRedirect.js
+++ portfolio/src/MusicRedirect.js
@@ -7,6 +7,13 @@ function MusicRedirect() {
       "https://www.youtube.com/watch?v=ZJM4AQSbZDk&ab_channel=MuraMasaVEVO",
       "https://www.youtube.com/watch?v=93ASUImTedo&ab_channel=DisclosureVEVO",
       "https://www.youtube.com/watch?v=tG35R8F2j8k&ab_channel=ChildishGambinoVEVO",
+      "https://www.youtube.com/watch?v=F6VfsJ7LAlE&ab_channel=LIVELOVEASAPVEVO",
+      "https://www.youtube.com/watch?v=q4GJVOMjCC4",
+      "https://www.youtube.com/watch?v=GxgqpCdOKak",
+      "https://www.youtube.com/watch?v=9Z55sZ2oVY4&ab_channel=MiguelVEVO",
+      "https://www.youtube.com/watch?v=rKlA5tRu6f0&ab_channel=KAYTRANADA"
+
+
       // Add more URLs as needed
     ];
 
