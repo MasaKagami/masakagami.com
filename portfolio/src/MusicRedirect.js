af6853b9c8a7e113744841338465ef7e5a67f620
MasaKagami <97225192+MasaKagami@users.noreply.github.com>
2024-12-02 07:14:50 -0500

    Update MusicRedirect.js

--- portfolio/src/MusicRedirect.js
+++ portfolio/src/MusicRedirect.js
@@ -4,14 +4,7 @@ function MusicRedirect() {
   useEffect(() => {
     // Array of YouTube video URLs
     const videos = [
-      "https://www.youtube.com/watch?v=ZJM4AQSbZDk&ab_channel=MuraMasaVEVO",
-      "https://www.youtube.com/watch?v=93ASUImTedo&ab_channel=DisclosureVEVO",
-      "https://www.youtube.com/watch?v=tG35R8F2j8k&ab_channel=ChildishGambinoVEVO",
-      "https://www.youtube.com/watch?v=F6VfsJ7LAlE&ab_channel=LIVELOVEASAPVEVO",
-      "https://www.youtube.com/watch?v=q4GJVOMjCC4",
-      "https://www.youtube.com/watch?v=GxgqpCdOKak",
-      "https://www.youtube.com/watch?v=9Z55sZ2oVY4&ab_channel=MiguelVEVO",
-      "https://www.youtube.com/watch?v=rKlA5tRu6f0&ab_channel=KAYTRANADA"
+      "https://soundcloud.com/jodyekagami"
 
 
       // Add more URLs as needed
